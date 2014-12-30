14588c4752b578b37104699511f0e1b9a1bfd8f7
justinvdm <justinvdm.0.1.0@gmail.com>
2014-12-30 10:47:46 +0200

    add native functions

--- chemist.js
+++ chemist.js
@@ -1,4 +1,24 @@
 ;(function() {
+  var _slice = Array.prototype.slice
+
+
+  var slice = fromNative(Array.prototype.slice)
+  var concat = fromNative(Array.prototype.concat)
+
+
+  function fromNative(nativeFn) {
+    return function(target) {
+      return nativeFn.apply(target, _slice.call(arguments, 1))
+    }
+  }
+
+
+  var chemist = {
+    slice: slice,
+    concat: concat
+  }
+
+
   if (typeof module != 'undefined')
     module.exports = chemist
   else if (typeof define == 'function' && define.amd)
--- chemist.test.js
+++ chemist.test.js
@@ -3,8 +3,31 @@ var vv = require('drainpipe')
 
 
 var chemist = require('./chemist'),
-    slice = chemist.slice,
+    slice = chemist.slice
 
 
 describe("chemist", function() {
+  describe("native functions", function() {
+    it("should wrap native functions", function() {
+      vv([1, 2, 3, 4])
+        (slice)
+        (assert.deepEqual, [1, 2, 3, 4])
+
+      vv([1, 2, 3, 4])
+        (slice, 1)
+        (assert.deepEqual, [2, 3, 4])
+
+      vv([1, 2, 3, 4])
+        (slice, 1, 3)
+        (assert.deepEqual, [2, 3])
+
+      vv([1, 2, 3, 4])
+        (slice, -2, 3)
+        (assert.deepEqual, [3])
+
+      vv([1, 2, 3, 4])
+        (slice, 1, -1)
+        (assert.deepEqual, [2, 3])
+    })
+  })
 })
